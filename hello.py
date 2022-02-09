0006b4b206bf70e81033c8a77809c8babcb48af2
shgopher <shgopher@foxmail.com>
2022-02-09 12:27:32 +0800

    feat(root): add function to hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
-print("hello shgopher!")
+print("hello shgopher!");
+print("hi there");
+
+def my_age(x):
+  if x > 12:
+    return "big boy"
+  else:
+    return "young boy"
+  
